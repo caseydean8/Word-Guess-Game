9101661c21ff4745fafcc3a5a5f49b021abfdeb5
Casey Carroll <caseydean8@gmail.com>
2019-06-02 15:16:34 -0400

    preliminary html framework

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var words = [['daenerys targaryen'], ['jon snow'], ['tyrion lannister'], ['bran stark'], ['sansa stark'], ['arya stark'], ['cersei lannister'], ['jaime lannister'], ['khal drago'], ['joffrey baratheon'], ['theon greyjoy'], ['samwell tarly'], ['brienne of tarth'], ['ramsay bolton'], ['podrick payne'], ['davos seaworth'], ['jorah mormont'], ['petyr Baelish']];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+}
+
+var remainingLetters = word.length;
