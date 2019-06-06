cfd9147b603f274cba267de9127169e85c00c2ec
Casey Carroll <caseydean8@gmail.com>
2019-06-06 09:06:50 -0400

    Added guesses left functionality.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// array of GOT names
+// array of GOT names, to be added after logic is completed
 // var words = ["daenerys targaryen", ['jon snow'], ['tyrion lannister'], ['bran stark'], ['sansa stark'], ['arya stark'], ['cersei lannister'], ['jaime lannister'], ['khal drago'], ['joffrey baratheon'], ['theon greyjoy'], ['samwell tarly'], ['brienne of tarth'], ['ramsay bolton'], ['podrick payne'], ['davos seaworth'], ['jorah mormont'], ['petyr Baelish']];
 
 var words = ['monkey', 'dog', 'pig']
@@ -18,21 +18,25 @@ for (var i = 0; i < word.length; i++) {
     document.getElementById("current-word").innerHTML = answerArray.join(" ");
 }
 }
-var guesses = 10
+
+// set guess-left counter
+var guesses = 9
+
 // get keypress to register
 document.onkeypress = function(key_dtl) {
     key_dtl = key_dtl || window.event; 
    var uni_code = key_dtl.keyCode || key_dtl.which; 
    var key_name = String.fromCharCode(uni_code);
     console.log(key_name);
+    document.getElementById("guesses-left").innerHTML = guesses--;
+    //if the word generated is equal to the players keypress then set variable letterInWord to true
     var letterInWord = false;
-    //if the generated randomword is equal to the letter entered... then variable is true
     for (var j = 0; j < word.length; j++) {
         if (word[j] === key_name) {
             letterInWord = true;
         }
     }
-    //if letterInWord (false)
+    //if letterInWord is true, create letter in current-word at correct index
     if (letterInWord) {
         console.log(letterInWord);
         for (var j = 0; j < word.length; j++){
@@ -44,10 +48,10 @@ document.onkeypress = function(key_dtl) {
         }
         
     }
-    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
+    // if letter in word is false, assign keypress to already-guessed
     else {
         document.getElementById("already-guessed").innerHTML += key_name;
-        guesses--;
+        
     }
 }
 
